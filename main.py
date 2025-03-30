c1f13fb0494854f77a76fce972835dcde7ebcc28
leonoriscoding <lmpbo1@iscte-iul.pt>
2025-03-30 18:46:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from mainwindow import FullScreenWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    
+    # Aplicar uma fonte global para toda a aplicação
+    from PyQt5.QtGui import QFont
+    app.setFont(QFont('Segoe UI', 11))
+    
     window = FullScreenWindow()
     sys.exit(app.exec_())
